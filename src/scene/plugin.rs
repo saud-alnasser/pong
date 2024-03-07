1bc484478bf96db8cbb249ff8efaff5ec1d7c47f
SAUD ALNASSER <saud4services@gmail.com>
2024-03-08 02:45:17 +0300

    feat: create basic ball

--- src/scene/plugin.rs
+++ src/scene/plugin.rs
@@ -1,11 +1,11 @@
 use bevy::prelude::*;
 
-use super::commands::spawn_camera;
+use super::commands::*;
 
 pub struct ScenePlugin;
 
 impl Plugin for ScenePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+        app.add_systems(Startup, (spawn_camera, spawn_ball));
     }
 }
