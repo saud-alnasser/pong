0ec56c6220a8d6dc493f2a9d98f05a66a89fddb1
SAUD ALNASSER <saud4services@gmail.com>
2024-03-08 03:33:37 +0300

    feat: create basic paddle

--- src/scene/commands.rs
+++ src/scene/commands.rs
@@ -1,7 +1,5 @@
-use bevy::{
-    prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+use bevy::prelude::*;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
 
 use super::components::*;
 
@@ -16,10 +14,36 @@ pub fn spawn_ball(
 ) {
     commands.spawn((
         MaterialMesh2dBundle {
-            mesh: Mesh2dHandle(meshes.add(Circle { radius: 20.0 })),
+            mesh: Mesh2dHandle(meshes.add(Circle::new(20.0))),
             material: materiels.add(Color::WHITE),
             ..default()
         },
         Ball,
+    ));
+}
+
+pub fn spawn_paddles(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(25.0, 200.0))),
+            material: materials.add(Color::WHITE),
+            transform: Transform::from_xyz(-500.0, 0.0, 0.0),
+            ..default()
+        },
+        Paddle,
+    ));
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(25.0, 200.0))),
+            material: materials.add(Color::WHITE),
+            transform: Transform::from_xyz(500.0, 0.0, 0.0),
+            ..default()
+        },
+        Paddle,
     ));
 }
