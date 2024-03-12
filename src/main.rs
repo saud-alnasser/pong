3e0175fb600451b24449121ed66b19cb555b5c47
SAUD ALNASSER <saud4services@gmail.com>
2024-03-12 07:59:09 +0300

    chore: use world inspector

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use bevy::prelude::*;
+use bevy::{input::common_conditions::input_toggle_active, prelude::*};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use pong::PongPlugins;
 
 fn main() {
-    App::new().add_plugins((DefaultPlugins, PongPlugins)).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Escape)),
+        )
+        .add_plugins(PongPlugins)
+        .run();
 }
