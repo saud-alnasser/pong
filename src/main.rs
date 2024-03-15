2da5c6ea351478201ec7562db2055eea03bfd4cf
saud alnasser <123410712+saud-alnasser@users.noreply.github.com>
2024-03-15 07:27:32 +0300

    feat: merge all code into lib.rs since the game is small (#15)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
-use bevy::{input::common_conditions::input_toggle_active, prelude::*};
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use pong::PongPlugins;
+use bevy::prelude::*;
+use pong::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Pong".to_string(),
-                resolution: (1280.0, 720.0).into(),
-                resizable: false,
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_plugins(
-            WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Escape)),
-        )
-        .add_plugins(PongPlugins)
-        .run();
+    App::new().add_plugins(PongPlugins).run();
 }
--- src/prelude.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::opponent::prelude::*;
-pub use crate::physics::prelude::*;
-pub use crate::player::prelude::*;
-pub use crate::scene::prelude::*;
-pub use crate::score::prelude::*;
